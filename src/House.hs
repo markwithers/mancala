ce15609cb2012299f4ea551d5f53244f8a21b3f6
Mark Withers <mark26266@gmail.com>
2015-09-24 21:36:54 +0100

    Slight tidy-up

--- src/House.hs
+++ src/House.hs
@@ -5,28 +5,29 @@ module House (
 type Stone = Int
 type Board = ([Stone], [Stone])
 
-play :: Board -> Int -> Either Board String
-play board position
-  | position < 0 || position >= length (fst board) = Right "Invalid Position"
-  | otherwise = Left $ sow (position + 1) (fst board !! position) . replace position 0 $ board
-
-replace :: Int -> Stone -> Board -> Board
-replace position value (mine, thiers)
-  | odd boardRem = (mine, rebuild value (splitAt (position - boardRem * 7) thiers))
-  | otherwise    = (rebuild value (splitAt (position - boardRem * 7) mine), thiers)
-  where boardRem = div position 7
-  
 rebuild :: Int -> ([Stone], [Stone]) -> [Stone]
 rebuild value (x,_:xs) = x ++ value : xs
 rebuild _ (_, []) = []
 
-sow :: Int -> Stone -> Board -> Board
-sow position 1 (mine, thiers)
-  | odd boardRem = replace position (thiers !! (position - boardRem * 7) + 1) (mine, thiers)
-  | otherwise    = replace position (mine !! (position - boardRem * 7) + 1) (mine, thiers)
-  where boardRem = div position 7
-sow position value board  = (sow (position + 1) (value - 1) . sow position 1) board
+play :: Board -> Int -> Either Board String
+play (b1, b2) position
+  | position < 0 || position >= length b1 = Right "Invalid Position"
+  | otherwise = Left
+    . sow (position + 1) (b1 !! position)
+    . replace position 0
+    $ (b1, b2)
 
---14 15 16 17 18 19 20
---7  8  9  10 11 12 s13
---0  1  2  3  4  5  s6
+replace :: Int -> Stone -> Board -> Board
+replace position value (b1, b2)
+  | odd quotient = (b1, rebuild value (splitAt offset b2))
+  | otherwise    = (rebuild value (splitAt offset b1), b2)
+  where quotient = div position 7
+        offset = position - quotient * 7
+
+sow :: Int -> Stone -> Board -> Board
+sow position 1 (b1, b2)
+  | odd quotient = replace position (b2 !! offset + 1) (b1, b2)
+  | otherwise = replace position (b1 !! offset + 1) (b1, b2)
+  where quotient = div position 7
+        offset = position - quotient * 7
+sow position value board = (sow (position + 1) (value - 1) . sow position 1) board

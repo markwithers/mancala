1914c0beed76e941f6ea23dd4f9b45ac9798682d
Mark Withers <mark26266@gmail.com>
2015-09-25 00:21:51 +0100

    Better naming

--- src/House.hs
+++ src/House.hs
@@ -5,29 +5,29 @@ module House (
 type Stone = Int
 type Board = ([Stone], [Stone])
 
-rebuild :: Int -> ([Stone], [Stone]) -> [Stone]
-rebuild value (x,_:xs) = x ++ value : xs
-rebuild _ (_, []) = []
+place :: Int -> ([Stone], [Stone]) -> [Stone]
+place stones (x,_:xs) = x ++ stones : xs
+place _ (_, []) = []
 
 sowRight :: Int -> [Stone] -> Board -> Board
-sowRight position row = sow (position + 1) (row !! position)
+sowRight position board = sow (position + 1) (board !! position)
 
 replace :: Int -> Stone -> [Stone] -> [Stone]
-replace position value row = rebuild value (splitAt position row)
+replace position stones board = place stones (splitAt position board)
 
 sow :: Int -> Stone -> Board -> Board
-sow position 1 (row1, row2)
-  | odd quotient = (row1, replace offset (row2 !! offset + 1) row2)
-  | otherwise = (replace offset (row1 !! offset + 1) row1, row2)
+sow position 1 (playerBoard, opponentBoard)
+  | odd quotient = (playerBoard, replace offset (opponentBoard !! offset + 1) opponentBoard)
+  | otherwise = (replace offset (playerBoard !! offset + 1) playerBoard, opponentBoard)
   where quotient = div position 7
         offset = position - quotient * 7
-sow position value board = sow (position + 1) (value - 1)
+sow position stones board = sow (position + 1) (stones - 1)
   . sow position 1
   $ board
 
 play :: Board -> Int -> Either Board String
-play (row1, row2) position
-  | position < 0 || position >= length row1 = Right "Invalid Position"
+play (playerBoard, opponentBoard) selectedHouse
+  | selectedHouse < 0 || selectedHouse >= length playerBoard = Right "Invalid Move!"
   | otherwise = Left
-    . sowRight position row1
-    $ (replace position 0 row1, row2)
+    . sowRight selectedHouse playerBoard
+    $ (replace selectedHouse 0 playerBoard, opponentBoard)

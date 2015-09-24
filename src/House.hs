3fade56a66d2da2ac5590fc7ece10c75a1b3c160
Mark Withers <mark26266@gmail.com>
2015-09-24 23:04:31 +0100

    Linting and consistency

--- src/House.hs
+++ src/House.hs
@@ -13,16 +13,16 @@ sowRight :: Int -> [Stone] -> Board -> Board
 sowRight position row = sow (position + 1) (row !! position)
 
 replace :: Int -> Stone -> Board -> Board
-replace position value (b1, b2)
-  | odd quotient = (b1, rebuild value (splitAt offset b2))
-  | otherwise    = (rebuild value (splitAt offset b1), b2)
+replace position value board
+  | odd quotient = (fst board, rebuild value (splitAt offset (snd board)))
+  | otherwise    = (rebuild value (splitAt offset (fst board)), snd board)
   where quotient = div position 7
         offset = position - quotient * 7
 
 sow :: Int -> Stone -> Board -> Board
-sow position 1 (b1, b2)
-  | odd quotient = replace position (b2 !! offset + 1) (b1, b2)
-  | otherwise = replace position (b1 !! offset + 1) (b1, b2)
+sow position 1 board
+  | odd quotient = replace position (snd board !! offset + 1) board
+  | otherwise = replace position (fst board !! offset + 1) board
   where quotient = div position 7
         offset = position - quotient * 7
 sow position value board = sow (position + 1) (value - 1)
@@ -34,5 +34,5 @@ play board position
   | position < 0 || position >= length (fst board) = Right "Invalid Position"
   | otherwise = Left
     . sowRight position (fst board)
-    . (replace position 0)
+    . replace position 0
     $ board
